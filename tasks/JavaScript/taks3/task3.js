9937f47bba49d8a9fc6bc8844b05ee2c366b0a7a
michauz <michauz@ac.sce.ac.il>
2020-10-07 15:38:58 +0300

    some tasks i found online

---
+++ tasks/JavaScript/taks3/task3.js
@@ -0,0 +1,4 @@
+//alert the value of the following filed: <input type="text" name="test" class="inputText" id="inputId" autocomplete="off" />
+function handle(){
+    alert(document.getElementById("inputId").value);
+}
